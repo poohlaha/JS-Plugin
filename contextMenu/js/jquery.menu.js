a01cd946cc5dad24befed9d35679c1c8fbf5fb61
ztyjr888 <ztyjr88@163.com>
2015-12-14 12:25:10 +0800

    Update jquery.menu.js

--- contextMenu/js/jquery.menu.js
+++ contextMenu/js/jquery.menu.js
@@ -190,6 +190,7 @@ var Map = (function (document) {
                     menu_ul_cls:'menu_ul',
                     menu_li_cls:'menu_li',
                     menu_li_a_cls:'menu_a',
+                    menu_arrow_cls:'menu_triangle',
                     onContextMenu:null
                 };
             },
@@ -277,6 +278,46 @@ var Map = (function (document) {
                 this._controller.proxy(func,this);
             },
 
+            _createMenuLi : function(data,isChildren,i,key){
+                var _menu_body_img = $('<img src="'+(data.img.src || this.defaultImageSrc)+'" width="'+(data.img.width || this.defaultImageWidth)+'" class="'+(data.img.cls || '')+'" height="'+(data.img.height || this.defaultImageHeight)+'" style="float: left">');
+                var _menu_body_a = $('<a href="javascript:;" class="'+(this.option.menu_li_a_cls)+'">'+(data.text || '')+'</a>');
+                var _menu_body_li = '';
+
+                if(isChildren){
+                    _menu_body_a.append($('<i class = "menu_triangle"></i>'));
+                }
+                if(key){
+                    _menu_body_li = $('<li class="'+this.option.menu_li_cls+'" tab-index = "'+(key)+'"></li>');
+                }else{
+                    _menu_body_li = $('<li class="'+this.option.menu_li_cls+'" tab-index = "'+(i + 1 + "_" + this._key)+'"></li>');
+                }
+
+                var _menu_body_li_div = $('<div class="picDiv"></div>');
+                _menu_body_li_div.append(_menu_body_img);
+                _menu_body_li_div.append(_menu_body_a);
+
+                _menu_body_li.append(_menu_body_li_div);
+                return _menu_body_li;
+            },
+
+            _bindLiClick : function(obj,map){
+                var self = this;
+                obj.find('li').each(function(){
+                    if($(this).hasClass(self.option.menu_li_cls)){
+                        if($(this).attr('tab-index')){
+                            var func = map.get($(this).attr('tab-index'));
+                            if(func){
+                                if(typeof func == "function"){
+                                    $(this).click(self._controller.proxy(func,self,undefined,function(){
+                                        obj.hide();
+                                    }));
+                                }
+                            }
+                        }
+                    }
+                });
+            },
+
             _showMenu : function(_left,_top){
                 if(this._getMenu.call(this)) return;
 
@@ -285,26 +326,49 @@ var Map = (function (document) {
                 var _menu_body_ul = $('<ul class="'+this.option.menu_ul_cls+'"></ul>');
 
                 var _map = new Map();
-                if(this.option.data && this.option.data.length > 0){
+                var _cMap = new Map();
+                var _cLiHover = new Array();
+                var _childrenBox = new Array();
 
+                if(this.option.data && this.option.data.length > 0){
                     for(var i = 0,len = this.option.data.length;i < len;i++){
                         var _data = this.option.data[i];
                         if(!_data) continue;
-                        if(!_data.img) _data.img = {};
-                        var _menu_body_img = $('<img src="'+(_data.img.src || this.defaultImageSrc)+'" width="'+(_data.img.width || this.defaultImageWidth)+'" class="'+(_data.img.cls || '')+'" height="'+(_data.img.height || this.defaultImageHeight)+'" style="float: left">');
-                        var _menu_body_a = '';
-                        var _func = _data.click;
-                        if(!_func || typeof _func !== "function")
-                            _menu_body_a = $('<a href="javascript:;" class="'+(this.option.menu_li_a_cls)+'">'+(_data.text || '')+'</a>');
-                        else
-                            _menu_body_a = $('<a href="javascript:;" class="'+(this.option.menu_li_a_cls)+'">'+(_data.text || '')+'</a>');
 
-                        var _menu_body_li = $('<li class="'+this.option.menu_li_cls+'" tab-index = "'+(i + 1 + "_" + this._key)+'"></li>');
-                        //_menu_body_a.click(this._executeFun.call(this,_func));
-                        _map.put( i + 1 +"_" + this._key,_func);
-                        _menu_body_li.append(_menu_body_img);
-                        _menu_body_li.append(_menu_body_a);
-                        _menu_body_ul.append(_menu_body_li);
+                        if(!_data.img) _data.img = {};
+
+                        //children
+                        if(_data.children && _data.children.length > 0){
+                            var _menu_box_c = $('<div class="'+this.option.menu_box_cls+'" data-key="'+(this._key + "_children_" + i)+'"></div>');
+                            var _menu_body_c = $('<div class="'+this.option.menu_body_cls+'"></div>');
+                            var _menu_body_ul_c = $('<ul class="'+this.option.menu_ul_cls+'"></ul>');
+
+                            for(var j = 0,cLen = _data.children.length;j < cLen;j++){
+                                var _cData = _data.children[j];
+                                if(!_cData) continue;
+
+                                if(!_cData.img) _cData.img = {};
+                                var _cDataKey = this._key + "_children_" + i +"_"+ (j + 1) +"_" + this._key+"_children_" + j;
+                                _menu_body_ul_c.append(this._createMenuLi.call(this,_cData,false,j,_cDataKey));
+
+                                _cMap.put(_cDataKey,_cData.click);
+                            }
+
+                            _menu_body_c.append(_menu_body_ul_c);
+                            _menu_box_c.append(_menu_body_c);
+                            var _menu_body_li = this._createMenuLi.call(this,_data,true,i);
+                            _menu_body_li.prepend(_menu_box_c);
+                            _menu_box_c.hide();
+                            _menu_body_ul.append(_menu_body_li);
+                            _childrenBox.push(_menu_box_c);
+                            _cLiHover.push(_menu_body_li);
+                        }else{
+                            _map.put( i + 1 +"_" + this._key,_data.click);
+                            //_menu_body_a.click(this._executeFun.call(this,_func));
+                            _menu_body_ul.append(this._createMenuLi.call(this,_data,false,i));
+                        }
+
+
                     }
                 }
 
@@ -312,21 +376,30 @@ var Map = (function (document) {
                 _menu_box.append(_menu_body);
 
                 if(!_map.isEmpty()){
-                    var self = this;
-                    _menu_box.find('li').each(function(){
-                        if($(this).hasClass(self.option.menu_li_cls)){
-                            if($(this).attr('tab-index')){
-                                var func = _map.get($(this).attr('tab-index'));
-                                if(func){
-                                    if(typeof func == "function"){
-                                        $(this).click(self._controller.proxy(func,self,undefined,function(){
-                                            _menu_box.hide();
-                                        }));
-                                    }
-                                }
-                            }
+                    this._bindLiClick.call(this,_menu_box,_map);
+                }
+
+                if(!_cMap.isEmpty()){
+                    if(_childrenBox && _childrenBox.length > 0){
+                        for(var i = 0,len = _childrenBox.length;i < len;i++){
+                            this._bindLiClick.call(this,_childrenBox[i],_cMap);
                         }
-                    });
+                    }
+                }
+
+                //bind hover event
+                if(_cLiHover && _cLiHover.length > 0){
+                    for(var i = 0,len = _cLiHover.length;i < len;i++){
+                        var _li = _cLiHover[i];
+                        if(!_li) continue;
+                        _li.hover(function(){
+                            $(this).find('.menu_box').show();
+                            $(this).addClass('menu_li_hover');
+                        },function(){
+                            $(this).find('.menu_box').hide();
+                            $(this).removeClass('menu_li_hover');
+                        });
+                    }
                 }
 
                 var _len = $("body").find('.menu_box').length;
